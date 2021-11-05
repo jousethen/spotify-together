6796f2ba7478f334294106273270a1c5447f0819
jousethen <jthen93@gmail.com>
2021-11-05 14:45:33 -0400

    Fixed env variables cause Im a dummy

--- src/test/test.js
+++ src/test/test.js
@@ -4,11 +4,7 @@ import 'react-spotify-auth/dist/index.css' // if using the included styles
 
 
 const Test = props => {
-  var client_id = '8211b083dda34f61a2f0dd2d8f4d70f6'; // Your client id
-  var client_secret = '2a24c1a3896141a0b1a09304b7f1a550'; // Your secret
-  var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
-
-
+  const client_secret = process.env.REACT_APP_CLIENT_SECRET; // Your secret
 
 
 
@@ -17,8 +13,8 @@ const Test = props => {
     <div>
       test
       <SpotifyAuth
-        redirectUri='http://localhost:3000/callback'
-        clientID='8211b083dda34f61a2f0dd2d8f4d70f6'
+        redirectUri={process.env.REACT_APP_REDIRECTURI}
+        clientID={process.env.REACT_APP_CLIENT_ID}
         scopes={[Scopes.userReadPrivate, Scopes.userReadEmail]}
       />
     </div>
