e2657da6491e85679339af924bb34c17a8c1b453
jousethen <jthen93@gmail.com>
2021-11-07 17:08:32 -0500

    Authentication Set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import Test from './test/test'
-function App() {
-  return (
-    <div className="App">
+import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import 'react-spotify-auth/dist/index.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Cookies from 'js-cookie';
 
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    const client_id = process.env.REACT_APP_CLIENT_ID;
+    const redirectUri = process.env.REACT_APP_REDIRECTURI;
+    console.log(Cookies.get())
+    return (
+      < div className="App" >
+        <SpotifyAuth
+          redirectUri={redirectUri}
+          clientID={client_id}
+          scopes={[Scopes.userReadPrivate, 'user-read-email', 'streaming', 'user-read-playback-state', 'user-modify-playback-state']} // either style will work
+          onAccessToken={(token) => Cookies.set("spotifyAuthToken", token)}
+        />
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { reducers } from "./reducers/index"
+import reducers from "./reducers/index"
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
