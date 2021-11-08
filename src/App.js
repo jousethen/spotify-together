242dd9de44818dd6ce02bdda46c673e972793bfd
jousethen <jthen93@gmail.com>
2021-11-07 20:12:21 -0500

    Login Page Complete. Need to add expire for token

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'react-spotify-auth/dist/index.css';
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Cookies from 'js-cookie';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
 
 class App extends Component {
 
@@ -11,15 +13,26 @@ class App extends Component {
   render() {
     const client_id = process.env.REACT_APP_CLIENT_ID;
     const redirectUri = process.env.REACT_APP_REDIRECTURI;
-    console.log(Cookies.get())
+
     return (
       < div className="App" >
-        <SpotifyAuth
-          redirectUri={redirectUri}
-          clientID={client_id}
-          scopes={[Scopes.userReadPrivate, 'user-read-email', 'streaming', 'user-read-playback-state', 'user-modify-playback-state']} // either style will work
-          onAccessToken={(token) => Cookies.set("spotifyAuthToken", token)}
-        />
+        {
+          Boolean(Cookies.get("spotifyAuthToken"))
+            ? <div>You are logged in</div> :
+            <div className="Login">
+              <Image src="./images/spotify-2-logo-png-transparent.png" roundedCircle width={100} />
+              <br />
+              <br />
+              <SpotifyAuth
+                title="Continue With Spotify"
+                redirectUri={redirectUri}
+                noLogo={true}
+                clientID={client_id}
+                scopes={[Scopes.userReadPrivate, 'user-read-email', 'streaming', 'user-read-playback-state', 'user-modify-playback-state']} // either style will work
+                onAccessToken={(token) => Cookies.set("", token)}
+              />
+            </div>
+        }
       </div >
     );
   }
