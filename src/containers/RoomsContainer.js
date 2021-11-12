7eb2af33dcdf6ba56cfb4bc18ee5a54af53e73ce
jousethen <jthen93@gmail.com>
2021-11-11 19:06:27 -0500

    Centered Room Create and Find buttons

--- src/containers/RoomsContainer.js
+++ src/containers/RoomsContainer.js
@@ -7,12 +7,12 @@ class RoomsContainer extends Component {
   render() {
 
     return (
-      <>
+      <div className="room_container">
         <ButtonGroup vertical className="center" >
           <Button variant="outline-light">Create Room</Button>
           <Button variant="outline-light">Join Room</Button>
         </ButtonGroup>
-      </>
+      </div>
     )
   }
 }
