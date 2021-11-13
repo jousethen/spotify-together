a5383941d05be9d347ec2355a9f220c1774eaf6a
jousethen <jthen93@gmail.com>
2021-11-13 02:27:02 -0500

    Front end sends request properly

--- src/actions/rooms.js
+++ src/actions/rooms.js
@@ -1,33 +1,34 @@
-import { createRoom } from './utility/spotifyApi'
-
 export const createRoom = (hostToken) => {
-  // Make POST request to create Room
-  dispatchEvent({ type: "CREATE_ROOM_START" });
-  let roomKey = generateRoomKey();
+  return (dispatch) => {
+    // Kick of dispatch to start room creation
+    dispatch({ type: "CREATE_ROOM_START" });
+    let roomKey = generateRoomKey();
 
-  fetch(`${process.env.REACT_APP_API_HOST}/api/rooms/new`, {
-    method: 'POST', headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      room: {
-        room_key: roomKey,
-        host_token: hostToken,
-        email: localStorage.getItem("spotifyUser"),
-      }
-    })
-  })
-    .then((responseJSON) => {
-      dispatchEvent({
-        type: "CREATE_ROOMS",
+    fetch(`${process.env.REACT_APP_API_HOST}/api/rooms/new`, {
+      method: 'POST', headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
         room: {
-          roomKey,
-          hostToken
+          room_key: roomKey,
+          host_token: hostToken,
+          email: localStorage.getItem("spotifyUser"),
         }
+      })
+    })
+      .then((responseJSON) => {
+        //Complete Dispatch of room creation
+        dispatch({
+          type: "CREATE_ROOMS",
+          room: {
+            roomKey,
+            hostToken
+          }
+        });
       });
-    });
-}
+  };
+};
 
 /* ----------------------------------------------------*/
 const generateRoomKey = () => {
